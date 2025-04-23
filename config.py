b6cf06123a8b810d6526b7abd43c54709ea8afbb
kevin Ubuntu 24.04 <contact@kevinh.dev>
2025-04-24 00:23:09 +1000

    update config

--- config.py
+++ config.py
@@ -9,6 +9,22 @@ import torch
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # --- Chess Game ---
+BOARD_SIZE = 8
+# 0–11  |   Current position: 6 piece types × 2 colors
+# 12–13 |   Repetition once / twice
+# 14–27 |   Position at t–1: same 14 planes
+# 28–41 |   Position at t–2: same 14 planes
+# … | …
+# 98–111|   Position at t–7
+# 112   |   Player‑to‑move (all 1’s if White, all 0’s if Black)
+# 113   |   White kingside castling right (1 or 0, everywhere)
+# 114   |   White queenside castling right
+# 115   |   Black kingside castling right
+# 116   |   Black queenside castling right
+# 117   |   Half‑move clock (no‑progress count)
+# 118   |   Full‑move number
+# 119   |   En passant
+INPUT_CHANNELS = 120
 NUM_ACTIONS = 8 * 8 * 73
 
 # --- MCTS ---
